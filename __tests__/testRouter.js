7f2a97dcef8adafae438df433fd269b2142976a1
Reed James <reedtjames1234@gmail.com>
2019-08-23 15:20:48 -0500

    Last commit, changing projects

--- __tests__/testRouter.js
+++ __tests__/testRouter.js
@@ -35,31 +35,56 @@ describe("server", () => {
     });
   });
   describe("POST /api/auth/login", () => {
-    it("should return 200 when a user is logging in", () => {
-      return (
-        request(server)
-          .post("/api/auth/login")
-          // .auth("username", "password")
+    // it("should return 200 when a user is logging in Async", async () => {
+    //   await request(server)
+    //     .post("/api/auth/login")
+    //     .send({
+    //       username: "test",
+    //       password: "pass"
+    //     });
+    //   const stat = await res.status;
 
-          //   .send({
-          //     username: "Reed1",
-          //     password: "password"
-          //   })
-          //   .send({
-          //     id: 1,
-          //     username: "Reed1",
-          //     password:
-          //       "$2a$12$ddjBRmjfnpfJgaIEGdiLI.U2.R24Z0U6d7k7F8OGjozeEvelDFM3m"
-          //   })
-          .send({
-            username: "test",
-            password: "pass"
-          })
-          .then(res => {
-            expect(res.status).toBe(200);
-          })
-      );
+    //   expect(stat).toBe(200);
+    // });
+    // it("succeeds with correct credentials", async () => {
+    //   const response = await request(server)
+    //     .post(`/api/auth/login`, { username: "test", password: "pass" })
+    //     .expect(200);
+    // });
+    it("succeeds with correct credentials", async () => {
+      const response = await request(server)
+        .post(`/api/auth/login`)
+        .send({ username: "test", password: "pass" })
+        .expect(200);
     });
+  });
+
+  describe("POST /api/auth/login", () => {
+    // it("should return 200 when a user is logging in", () => {
+    //   return (
+    //     request(server)
+    //       .post("/api/auth/login")
+    //       // .auth("username", "password")
+
+    //       //   .send({
+    //       //     username: "Reed1",
+    //       //     password: "password"
+    //       //   })
+    //       //   .send({
+    //       //     id: 1,
+    //       //     username: "Reed1",
+    //       //     password:
+    //       //       "$2a$12$ddjBRmjfnpfJgaIEGdiLI.U2.R24Z0U6d7k7F8OGjozeEvelDFM3m"
+    //       //   })
+    //       .send({
+    //         username: "test",
+    //         password: "pass"
+    //       })
+    //       .then(res => {
+    //         expect(res.status).toBe(200);
+    //       })
+    //   );
+    // });
     it("should return JSON to login", () => {
       return request(server)
         .post("/api/auth/login")
